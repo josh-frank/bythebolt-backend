2fba67a7f71006ba53a63f81a719ab74f2dcf5fc
Josh Frank <eurosewusa@gmail.com>
2021-03-21 22:35:41 -0400

    working create and destroy routes/actions for FavoriteListing

--- app/controllers/favorite_listings_controller.rb
+++ app/controllers/favorite_listings_controller.rb
@@ -1,9 +1,15 @@
 class FavoriteListingsController < ApplicationController
 
+    before_action :authenticate, only: [ :create, :destroy ]
+
     def create
+        FavoriteListing.create( user_id: @current_user.id, listing_id: params[ :listing_id ] )
+        render json: @current_user
     end
 
     def destroy
+        FavoriteListing.find( params[ :id ] ).destroy
+        render json: @current_user
     end
 
 end
